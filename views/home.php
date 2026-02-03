a31f79b72be6b60f416a80331e9f21644505fb58
guillaumepeixotoduarte <guillaume.peixotoduarte@gmail.com>
2026-02-03 11:54:41 +0100

    Modif homepage

--- views/home.php
+++ views/home.php
@@ -1,44 +1,50 @@
 <?php include_once ROOT. '/views/layouts/header.php'; ?>
 
 <section class="main-bg-color">
-    <div class="max-width-1000 px-2 py-5 w-80 mx-auto d-flex align-items-center">
-        <div class="section-text-content">
-            <h1>Rejoingez nos lecteurs passionnés</h1>
-            <p>Donnez une nouvelle vie à vos livres en les échangeant avec d'autres amoureux de la lecture. Nous croyons en la magie du partage de connaissances et d'histoires à travers les livres. </p>
-            <button class="classic-button green-button mt-3">Découvrir</button>
-        </div>
-        <div class="section-image-content">
-            <img id="home-image" src="img/home_image.png" alt="Echange de livres">
-            <p class="italic-text text-right">Hamza</p>
-        </div>
+    <div class="container-fluid px-0 mx-auto px-md-2 py-5 d-flex align-items-center " style="max-width: 1000px;">
+        <div class="row w-100 m-0 flex-wrap-reverse"> 
+            <div class="section-text-content col-12 col-lg-6 px-4 px-sm-0 mt-5 mt-lg-auto">
+                <h1>Rejoignez nos lecteurs passionnés</h1>
+                <p>Donnez une nouvelle vie à vos livres en les échangeant avec d'autres amoureux de la lecture. Nous croyons en la magie du partage de connaissances et d'histoires à travers les livres. </p>
+                <button class="classic-button green-button mt-3 w-100 w-sm-auto">Découvrir</button>
+            </div>
+
+            <div class="col-12 col-lg-6 d-flex flex-column align-items-center align-items-lg-end p-0">
+                <div class="image-wrapper">
+                    <img id="home-image" src="img/home_image.png" alt="Echange de livres" class="img-fluid"> 
+                    <p class="italic-text text-end w-100 my-2 mx-0 pe-2 pe-sm-0">Hamza</p>
+                </div>
+            </div>
+            
+        </div>  
     </div>
 </section>
 
 <section class="second-bg-color text-center ">
-    <div class="max-width-1000 w-80 mx-auto py-5">
+    <div class="max-width-1000 w-80 mx-auto px-4 px-lg-0 py-5">
         <h2> Les derniers livres ajoutés </h2>
         <div id="latest-books" class="">
             <!-- Les livres seront chargés ici dynamiquement -->
         </div>
-        <button class="classic-button green-button mt-3">Voir tous les livres</button>
+        <button class="classic-button green-button mt-3 w-100 w-sm-auto">Voir tous les livres</button>
     </div>
 </section>
 
 <section class="main-bg-color">
-    <div class="text-center py-5 px-2 px-lg-0 max-width-1000 w-80 mx-auto">
+    <div class="text-center py-5 px-4 px-lg-0 max-width-1000 w-80 mx-auto">
         <h2 class="py-4">Comment ça marche ?</h2>
         <p>Échanger des livres avec TomTroc c’est simple et <br> amusant ! Suivez ces étapes pour commencer :</p>
-        <div class="d-flex justify-content-between  flex-column flex-sm-row flex-nowrap flex-sm-wrap flex-lg-nowrap mt-4">
+        <div class="d-flex justify-content-lg-around justify-content-between flex-column flex-sm-row flex-nowrap flex-sm-wrap flex-lg-nowrap mt-4">
             <div class="bg-white d-flex rounded-3 home-help-blocs my-2"><p class="m-auto">Inscrivez-vous <br> gratuitement sur <br> notre plateforme.</p></div>
             <div class="bg-white d-flex rounded-3 home-help-blocs my-2"><p class="m-auto">Ajoutez les livres que vous <br> souhaitez échanger à <br> votre profil.</p></div>
             <div class="bg-white d-flex rounded-3 home-help-blocs my-2"><p class="m-auto">Parcourez les livres <br> disponibles chez d'autres <br> membres.</p></div>
             <div class="bg-white d-flex rounded-3 home-help-blocs my-2"><p class="m-auto">Proposez un échange et <br> discutez avec d'autres <br> passionnés de lecture.</p></div>
         </div>
-        <button class="classic-button secondary-green-button mt-3">Voir tous les livres</button>
+        <button class="classic-button secondary-green-button mt-3 w-100 w-sm-auto">Voir tous les livres</button>
     </div>
     <img class="d-none d-sm-block w-100 object-fit-cover" src="img/second_home_image.png" alt="Echange de livres">
     <img class="second-home-image w-100 d-sm-none" src="img/second_home_phone_image.png" alt="Echange de livres">
-    <div id="nos-valeurs" class="py-5 px-2 px-lg-0 mx-auto">
+    <div id="nos-valeurs" class="py-5 px-4 px-lg-0 mx-auto">
         <h2 class="mb-4">Nos valeurs</h2>
         <p>
             Chez Tom Troc, nous mettons l'accent sur le partage, la découverte et la communauté. 
