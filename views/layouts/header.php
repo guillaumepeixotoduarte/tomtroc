026fb25c4a18287845c1463c312de694fa2fed14
guillaumepeixotoduarte <guillaume.peixotoduarte@gmail.com>
2026-02-04 15:31:14 +0100

    Modif menu

--- views/layouts/header.php
+++ views/layouts/header.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title><?= $title ?? 'Mon Site' ?></title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.13.1/font/bootstrap-icons.min.css" integrity="sha512-t7Few9xlddEmgd3oKZQahkNI4dS6l80+eGEzFQiqtyVYdvcSG2D3Iub77R20BdotfRPA9caaRkg1tyaJiPmO0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
         <link rel="stylesheet" href="css/style.css">
     </head>
     <body>
@@ -32,24 +33,24 @@
                         </ul>
 
                         <?php if(!empty($_SESSION['user'])): ?>
-                            <ul class="navbar-nav">
-                                <li class="nav-item">
-                                    <a class="nav-link" href="<?= url('messages') ?>">Messagerie</a>
+                            <ul class="navbar-nav me-5 pe-5">
+                                <li class="nav-item mx-3">
+                                    <a class="nav-link <?= navBarIsActive('messages') ?>" href="<?= url('messages') ?>"><i class="bi bi-chat"></i> Messagerie</a>
                                 </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="<?= url('profil') ?>">Mon Profil</a>
+                                <li class="nav-item mx-3">
+                                    <a class="nav-link <?= navBarIsActive('profile') ?>" href="<?= url('profile') ?>"><i class="bi bi-person-circle"></i> Mon compte</a>
                                 </li>
-                                <li class="nav-item">
+                                <li class="nav-item mx-3">
                                     <a class="nav-link" href="<?= url('logout') ?>">Déconnexion</a>
                                 </li>
                             </ul>
                         <?php else: ?>
-                            <ul class="navbar-nav">
-                                <li class="nav-item">
-                                    <a class="nav-link" href="<?= url('inscription') ?>">Inscription</a>
+                            <ul class="navbar-nav me-5 pe-5">
+                                <li class="nav-item mx-3">
+                                    <a class="nav-link <?= navBarIsActive('inscription') ?>" href="<?= url('inscription') ?>">Inscription</a>
                                 </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="<?= url('login') ?>">Connexion</a>
+                                <li class="nav-item mx-3">
+                                    <a class="nav-link <?= navBarIsActive('login') ?>" href="<?= url('login') ?>">Connexion</a>
                                 </li>
                             </ul>
                         <?php endif; ?>
