c9846539b5ddd37c4bbfde0d9e7e4eab23f1b40b
guillaumepeixotoduarte <guillaume.peixotoduarte@gmail.com>
2026-02-12 10:28:05 +0100

    Modif footer

--- views/layouts/footer.php
+++ views/layouts/footer.php
@@ -1,10 +1,10 @@
         </main> 
 
-        <footer class="text-center font-size-12 d-flex flex-row justify-content-end align-items-center">
-            <span class="me-0 me-md-5">Politique de confidentialité</span>
-            <span class="me-0 me-md-5">Mentions légales</span>
-            <span class="me-0 me-md-5">Tom Troc©</span>
-            <img class="me-0 me-md-5" src="<?= url('img/logo-footer.png') ?>" alt="Logo Tom Troc">
+        <footer class="text-center font-size-12 d-flex flex-column flex-md-row justify-content-end align-items-center">
+            <span class="me-0 my-3 my-md-0 me-md-5">Politique de confidentialité</span>
+            <span class="me-0 mb-3 mb-md-0 me-md-5">Mentions légales</span>
+            <span class="me-0 mb-3 mb-md-0 me-md-5">Tom Troc©</span>
+            <img class="me-0 mb-3 mb-md-0 me-md-5" src="<?= url('img/logo-footer.png') ?>" alt="Logo Tom Troc">
         </footer>
 
         <script src="<?= url('js/script.js') ?>"></script>
